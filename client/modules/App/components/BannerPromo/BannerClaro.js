d7e66689e2a4734caf17a437fcd9bc90b9a0da81
Jorge Useche <naturalmentejorge@gmail.com>
2017-08-15 01:43:50 -0500

    Banner promo

--- client/modules/App/components/BannerPromo/BannerClaro.js
+++ client/modules/App/components/BannerPromo/BannerClaro.js
@@ -4,33 +4,27 @@ import React from 'react';
 import styles from './BannerClaro.css';
 
 // Import Images
-import imgFondo from './img/claro.png';
+import imgClaro from './img/claro-logo.png';
 
 export function BannerClaro(props) {
   return (
-    <div className={"container-fluid " + styles.marco}>
-      <div className="row">
-        <div className="col-sm-6 col-md-3">
-          <div>
-            <img src={imgFondo} alt="claro" className="center-block"/>
-          </div>
+    <div className={"col-xs-12 " + styles.marco}>
+      <div className={styles.mainContent + " " + styles['text-center']}>
+        <h3 className={styles.mB40px}>NUESTROS PARTNERS</h3>
+        <div className={"col-sm-3 col-xs-6 " + " " + styles['text-center']}>
+          <img className={"img-responsive " + styles.mA} src={imgClaro} alt=""/>
         </div>
-        <div className="col-sm-6 col-md-3">
-          <div>
-            <img src={imgFondo} alt="claro" className="center-block"/>
-          </div>
+        <div className={"col-sm-3 col-xs-6 " + styles['text-center']}>
+          <img className={"img-responsive " + styles.mA} src={imgClaro} alt=""/>
         </div>
-        <div className="col-sm-6 col-md-3">
-          <div>
-            <img src={imgFondo} alt="claro" className="center-block"/>
-          </div>
+        <div className={"col-sm-3 col-xs-6 " + styles['text-center']}>
+          <img className={"img-responsive " + styles.mA} src={imgClaro} alt=""/>
         </div>
-        <div className="col-sm-6 col-md-3">
-          <div>
-            <img src={imgFondo} alt="claro" className="center-block"/>
-          </div>
+        <div className={"col-sm-3 col-xs-6 " + styles['text-center']}>
+          <img className={"img-responsive " + styles.mA} src={imgClaro} alt=""/>
         </div>
       </div>
+      <div className={"col-xs-12 " + styles.h40px}></div>
     </div>
   );
 }
